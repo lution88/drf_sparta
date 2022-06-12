2e71f7eaa3f5c9a7d00fe631c352a085dc9c32aa
lution88 <lution88@gmail.com>
2022-06-12 16:42:19 +0900

    delete homework folder

--- homework/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# from homework.models import Place, Restaurant, Waiter
-
-# # Register your models here.
-
-# admin.site.register(Place)
-# admin.site.register(Restaurant)
-# admin.site.register(Waiter)
